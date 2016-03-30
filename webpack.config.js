856c00e72ea24a555790312137cd3df63270e53c
niahoo <dev@ooha.in>
2016-03-30 11:27:29 +0200

    move js apps to prepare for multiple apps (editor / pickr)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 var webpack = require("webpack")
 var extend = require("extend")
-var dotenv = require('dotenv')
 
-console.log("@todo closure compile & test")
-
-
-dotenv.load()
-console.log('APP_DEBUG=', process.env.APP_DEBUG)
-
-function env(x) {
-    return process.env[x]
+function pickrPath(sub) {
+    return "./jsapps/pickr" + (sub || '')
 }
 
-function appfile(file) {
-    return "./pickrjs/" + file
+function editorPath(sub) {
+    return "./jsapps/editor" + (sub || '')
+}
+
+function commonPath(sub) {
+    return "./jsapps/common" + (sub || '')
+}
+
+function distPath(sub) {
+    return "./priv/static/js/bundle" + (sub || '')
 }
 
 function pickrConfig(specific) {
     var common = {
         entry: {
-            "pizza-picker": appfile("js/picker.js"),
-            "pizza-picker-lc_fr": appfile("js/lc/fr.js")
+            "pizza-picker": pickrPath("/js/picker.js"),
+            "pizza-picker-lc_fr": pickrPath("/js/lc/fr.js")
         },
         module: {
             preLoaders: [],
@@ -40,7 +41,36 @@ function pickrConfig(specific) {
             ]
         },
         resolve: {
-            modulesDirectories: ['pickrjs/js', 'node_modules']
+            modulesDirectories: [commonPath('/js'), pickrPath('/js'), 'node_modules']
+        },
+    }
+    return extend(common, specific)
+}
+function editorConfig(specific) {
+    var common = {
+        entry: {
+            "editor": editorPath("/js/editor.js"),
+            "editor-lc_fr": editorPath("/js/lc/fr.js")
+        },
+        module: {
+            preLoaders: [],
+            loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ["mjsx"]
+                }
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+            ]
+        },
+        resolve: {
+            modulesDirectories: [commonPath('/js'), editorPath('/js'), 'node_modules']
         },
     }
     return extend(common, specific)
@@ -50,11 +80,16 @@ var configs = {}
 
 configs.pickr_dev = pickrConfig({
     output: {
-        path: "dist",
+        path: distPath(),
         filename: "[name].js"
     },
     devtool: 'eval-source-map',
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        }),
         new webpack.optimize.OccurenceOrderPlugin(true),
         new webpack.optimize.DedupePlugin()
     ]
@@ -62,13 +97,36 @@ configs.pickr_dev = pickrConfig({
 
 configs.pickr_prod = pickrConfig({
     output: {
-        path: "dist",
+        path: distPath(),
         filename: "[name].min.js"
     },
     devtool: 'cheap-module-source-map',
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.OccurenceOrderPlugin(true),
         new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new webpack.optimize.DedupePlugin()
+    ]
+})
+
+
+configs.editor_dev = editorConfig({
+    output: {
+        path: distPath(),
+        filename: "[name].js"
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(true),
         new webpack.optimize.DedupePlugin()
     ]
 })
